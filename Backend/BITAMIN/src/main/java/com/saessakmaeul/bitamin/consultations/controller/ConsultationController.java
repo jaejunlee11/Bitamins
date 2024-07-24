d6ae450502d07f46138c481da1a338f90c094dc1
SeongJeong <jsj046@naver.com>
2024-07-24 16:33:48 +0900

    feat: 실시간 채팅 조회 및 발신 기능

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/consultations/controller/ConsultationController.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/consultations/controller/ConsultationController.java
@@ -2,15 +2,13 @@ package com.saessakmaeul.bitamin.consultations.controller;
 
 import com.saessakmaeul.bitamin.consultations.Entity.SearchCondition;
 import com.saessakmaeul.bitamin.consultations.dto.request.*;
-import com.saessakmaeul.bitamin.consultations.dto.response.JoinRandomResponse;
-import com.saessakmaeul.bitamin.consultations.dto.response.JoinRoomResponse;
-import com.saessakmaeul.bitamin.consultations.dto.response.RegistRoomResponse;
-import com.saessakmaeul.bitamin.consultations.dto.response.SelectAllResponse;
+import com.saessakmaeul.bitamin.consultations.dto.response.*;
 import com.saessakmaeul.bitamin.consultations.service.ConsultationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -122,4 +120,28 @@ public class ConsultationController {
         return ResponseEntity.status(200).body("정상적으로 퇴장 처리 되었습니다.");
     }
 
+    @GetMapping("/chaings/{consultationId}")
+    public ResponseEntity<?> findById(@PathVariable("consultationId") Long consultationId) {
+        List<FindByIdResponse> chatingList = consultationService.findById(consultationId);
+
+        return ResponseEntity.status(200).body(chatingList);
+    }
+
+    @PostMapping("/chatings")
+    public ResponseEntity<?> registChating(
+//            @RequestHeader(value = "Authorization", required = false) String tokenHeader,
+            @RequestBody RegistChatingRequest registChatingRequest) {
+
+//        String memberId = jwtUtil.getIdFromToken(tokenHeader.substring(7));
+//        String memberNickname = jwtUtil.getNicknameFromToken(tokenHeader.substring(7));
+
+//        registChatingRequest.setMemberId(memberId);
+//        registChatingRequest.setMemberNickname(memberNickname);
+
+        int result = consultationService.registChating(registChatingRequest);
+
+        if(result == 0) ResponseEntity.status(404).body("채팅이 저장되지 않았습니다.");
+
+        return ResponseEntity.status(200).body("정상적으로 채팅이 저장되었습니다.");
+    }
 }
