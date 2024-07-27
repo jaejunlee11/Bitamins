506ce3377ea95e20e010759cb2fc6718d614a0d5
jaejunlee11 <sktks11@naver.com>
2024-07-28 02:03:22 +0900

    feat: #22-신고 리스트 조회 기능 구현 완료

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/complaint/controller/ComplaintController.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/complaint/controller/ComplaintController.java
@@ -1,10 +1,36 @@
 package com.saessakmaeul.bitamin.complaint.controller;
 
+import com.saessakmaeul.bitamin.complaint.dto.responseDto.ComplaintSimpleResponse;
+import com.saessakmaeul.bitamin.complaint.service.ComplaintService;
+import com.saessakmaeul.bitamin.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/complaints")
 public class ComplaintController {
+
+    @Autowired
+    private ComplaintService complaintService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @GetMapping("")
+    public ResponseEntity<?> getComplaintList(@RequestHeader(name = "Authorization", required = false) String token){
+        try{
+            long userId =jwtUtil.extractUserId(token.substring(7));
+            List<ComplaintSimpleResponse> responseList = complaintService.getComplaintList(userId);
+            return ResponseEntity.ok(responseList);
+        } catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 
 }
