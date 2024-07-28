3ee297361470cb4c51717d4bdc1c1e9f47dea25a
jaejunlee11 <sktks11@naver.com>
2024-07-28 13:40:46 +0900

    feat: #23-신고 디테일 조회 기능 구현 완료

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/complaint/controller/ComplaintController.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/complaint/controller/ComplaintController.java
@@ -1,14 +1,12 @@
 package com.saessakmaeul.bitamin.complaint.controller;
 
 import com.saessakmaeul.bitamin.complaint.dto.responseDto.ComplaintSimpleResponse;
+import com.saessakmaeul.bitamin.complaint.dto.responseDto.ComplatinDetailResponse;
 import com.saessakmaeul.bitamin.complaint.service.ComplaintService;
 import com.saessakmaeul.bitamin.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,6 +26,17 @@ public class ComplaintController {
             long userId =jwtUtil.extractUserId(token.substring(7));
             List<ComplaintSimpleResponse> responseList = complaintService.getComplaintList(userId);
             return ResponseEntity.ok(responseList);
+        } catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getComplaintDetail(@RequestHeader(name = "Authorization", required = false) String token, @PathVariable(name = "id") long id) {
+        try {
+            long userId = jwtUtil.extractUserId(token.substring(7));
+            ComplatinDetailResponse response = complaintService.getComplaintDetail(id, userId);
+            return ResponseEntity.ok(response);
         } catch (Exception e){
             return ResponseEntity.badRequest().body(e.getMessage());
         }
