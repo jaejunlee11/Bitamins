55ead69af5f3d14de9009a1b2f7b5facb6c7d726
hyun <a94460093@gmail.com>
2024-07-29 15:18:07 +0900

    refactor: access token, refresh token 저장 변경

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/controller/AuthController.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/member/controller/AuthController.java
@@ -8,6 +8,10 @@ import com.saessakmaeul.bitamin.member.repository.MemberRepository;
 import com.saessakmaeul.bitamin.member.repository.RefreshTokenRepository;
 import com.saessakmaeul.bitamin.util.JwtUtil;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +21,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
@@ -25,6 +28,8 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
+@Tag(name = "Auth Controller", description = "권한, 토큰 관리하는 컨트롤러")
 public class AuthController {
 
     private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
@@ -43,16 +48,19 @@ public class AuthController {
 
     @Operation(summary = "로그인", description = "로그인 시 AccessToken, RefreshToken 생성 && 기존의 RefreshToken이 존재한다면 토큰 업데이트")
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest, HttpServletResponse response) {
         try {
             Authentication authentication = authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword())
             );
             SecurityContextHolder.getContext().setAuthentication(authentication);
-            Member user = memberRepository.findByEmail(loginRequest.getEmail()).orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
+
+            Member user = memberRepository.findByEmail(loginRequest.getEmail())
+                    .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
+
             String jwt = jwtUtil.generateAccessToken(user);
             String refreshToken = jwtUtil.generateRefreshToken(user);
-            // 기존 RefreshToken이 있으면 업데이트, 없으면 생성
+
             Optional<RefreshToken> existingToken = refreshTokenRepository.findById(user.getId());
             RefreshToken token;
             if (existingToken.isPresent()) {
@@ -66,6 +74,15 @@ public class AuthController {
                 token.setUser(user);
             }
             refreshTokenRepository.save(token);
+
+            response.setHeader("Authorization", "Bearer " + jwt);
+
+            Cookie refreshTokenCookie = new Cookie("refresh_token", refreshToken);
+            refreshTokenCookie.setHttpOnly(true);
+            refreshTokenCookie.setPath("/");
+            refreshTokenCookie.setMaxAge((int) jwtUtil.getRefreshTokenExpiration() / 1000); // 초 단위로 설정
+            response.addCookie(refreshTokenCookie);
+
             return ResponseEntity.ok(new AuthResponse(jwt, refreshToken, true));
         } catch (Exception e) {
             logger.error("로그인 오류: ", e);
@@ -73,24 +90,34 @@ public class AuthController {
         }
     }
 
-    @Operation(summary = "AccessToken 재생성", description = "AccessToken에서 사용자 ID를 추출하여 RefreshToken을 자동으로 검색하고, 유효하다면 AcceessToken을 재생성")
+
+    @Operation(summary = "AccessToken 재생성", description = "쿠키의 RefreshToken을 DB의 RefreshToken과 비교하여 유효하다면 AccessToken을 재생성")
     @PostMapping("/refresh-token")
-    public ResponseEntity<?> refreshToken() {
+    public ResponseEntity<?> refreshToken(HttpServletRequest request, HttpServletResponse response) {
         try {
-            // SecurityContextHolder를 통해 AccessToken 가져오기
-            UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-            Long userId = jwtUtil.extractUserIdFromPrincipal(userDetails);
-            // 사용자 ID로 RefreshToken 검색
-            Optional<RefreshToken> refreshToken = refreshTokenRepository.findByUserId(userId);
-            if (refreshToken.isPresent()) {
-                String requestRefreshToken = refreshToken.get().getToken();
-                if (!jwtUtil.isTokenExpired(requestRefreshToken)) {
-                    String newToken = jwtUtil.generateAccessToken(memberRepository.findById(userId).orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다.")));
-                    return ResponseEntity.ok(new AuthResponse(newToken, requestRefreshToken, true));
-                } else {
-                    logger.info("만료된 Refresh Token: {}", requestRefreshToken);
-                    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Refresh Token이 만료되었습니다.");
+            String cookieRefreshToken = null;
+            Cookie[] cookies = request.getCookies();
+            if (cookies != null) {
+                for (Cookie cookie : cookies) {
+                    if ("refresh_token".equals(cookie.getName())) {
+                        cookieRefreshToken = cookie.getValue();
+                    }
                 }
+            }
+            if (cookieRefreshToken == null) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Refresh Token이 쿠키에 존재하지 않습니다.");
+            }
+            if (jwtUtil.isTokenExpired(cookieRefreshToken)) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Refresh Token이 만료되었습니다.");
+            }
+            Long userId = jwtUtil.extractUserId(cookieRefreshToken);
+            Optional<RefreshToken> refreshToken = refreshTokenRepository.findByUserId(userId);
+            if (refreshToken.isPresent() && refreshToken.get().getToken().equals(cookieRefreshToken)) {
+                String newAccessToken = jwtUtil.generateAccessToken(memberRepository.findById(userId)
+                        .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다.")));
+
+                response.setHeader("Authorization", "Bearer " + newAccessToken);
+                return ResponseEntity.ok(new AuthResponse(newAccessToken, cookieRefreshToken, true));
             } else {
                 return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("유효한 Refresh Token이 없습니다.");
             }
@@ -99,6 +126,7 @@ public class AuthController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("AccessToken 재생성 실패: " + e.getMessage());
         }
     }
+
 
     @Operation(summary = "로그아웃 테스트", description = "로그아웃 시 RefreshToken 삭제 없이 처리")
     @PostMapping("/logout")
@@ -112,4 +140,30 @@ public class AuthController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("로그아웃 실패: " + e.getMessage());
         }
     }
+
+    @Operation(summary = "회원 권한 조회", description = "AccessToken 파싱해서 회원 권한 조회")
+    @GetMapping("/role")
+    public ResponseEntity<String> getUserRole(HttpServletRequest request) {
+        try {
+            String token = getTokenFromRequest(request);
+            if (token == null) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
+
+            String role = jwtUtil.extractRole(token);
+            return ResponseEntity.ok(role);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @Operation(summary = "JWT 토큰 추출 메서드", description = "헤더에서 JWT 토큰을 추출하는 메서드")
+    private String getTokenFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
 }
