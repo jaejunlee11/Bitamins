410903877bf2046647812413503441dfb9ed0d20
이재준 <sktks11@gmail.com>
2024-08-05 09:45:42 +0900

    feat: #51-CORS 구현 완료

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/config/WebConfig.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/config/WebConfig.java
@@ -4,18 +4,20 @@ import com.saessakmaeul.bitamin.consultation.Entity.SearchCondition;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("http://localhost:5173", "http://localhost:3000",
-//                        "http://172.30.1.57:3000")
-//                .allowedMethods("GET","POST","PUT","DELETE","PATCH","OPTIONS");
-//    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://i11b105.p.ssafy.io","http://i11b105.p.ssafy.io","http://localhost:3000","https://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "PATCH","DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .maxAge(3000);
+    }
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
