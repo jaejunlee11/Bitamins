4417112187b80f31ec8d4d9f48171025215fe6fa
이재준 <sktks11@gmail.com>
2024-08-05 16:07:52 +0900

    hotfix: #51-CORS

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/config/WebConfig.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/config/WebConfig.java
@@ -1,25 +1,28 @@
 package com.saessakmaeul.bitamin.config;
 
 import com.saessakmaeul.bitamin.consultation.Entity.SearchCondition;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://i11b105.p.ssafy.io","http://i11b105.p.ssafy.io","http://localhost:5173","https://localhost:5173","http://localhost:5174","https://localhost:5174","http://127.0.0.1:5173","https://127.0.0.1:5173")
-                .allowedMethods("GET", "POST", "PUT", "PATCH","DELETE", "OPTIONS")
-                .allowedHeaders("Content-Type", "Authorization")
-                .allowCredentials(true)
-                .maxAge(3000);
+        registry
+                .addMapping("/**")
+                .allowedOrigins("http://localhost:5743","http://localhost:3000", "https://i11b303.p.ssafy.io")
+                .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name(), HttpMethod.HEAD.name(), HttpMethod.OPTIONS.name(),
+                        HttpMethod.PATCH.name())
+                .maxAge(1800); // Pre-flight Caching
     }
+
+
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
