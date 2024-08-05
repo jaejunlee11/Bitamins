2143401abcc4734cff27348afdeca1f72857071a
이재준 <sktks11@gmail.com>
2024-08-05 14:48:20 +0900

    hotfix: #51-CORS

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/config/WebConfig.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/config/WebConfig.java
@@ -1,13 +1,25 @@
 package com.saessakmaeul.bitamin.config;
 
 import com.saessakmaeul.bitamin.consultation.Entity.SearchCondition;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://i11b105.p.ssafy.io","http://i11b105.p.ssafy.io","http://localhost:5173","https://localhost:5173","http://localhost:5174","https://localhost:5174","http://127.0.0.1:5173","https://127.0.0.1:5173")
+                .allowedMethods("GET", "POST", "PUT", "PATCH","DELETE", "OPTIONS")
+                .allowedHeaders("Content-Type", "Authorization")
+                .allowCredentials(true)
+                .maxAge(3000);
+    }
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
