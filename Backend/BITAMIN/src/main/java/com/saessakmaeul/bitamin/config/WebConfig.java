af04ef5c0e581af089eaad7ab1a3170a7168e5e1
이재준 <sktks11@gmail.com>
2024-08-05 14:18:22 +0900

    hotfix: #51-CORS

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/config/WebConfig.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/config/WebConfig.java
@@ -4,31 +4,10 @@ import com.saessakmaeul.bitamin.consultation.Entity.SearchCondition;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173")); // 허용할 도메인 명시
-        configuration.addAllowedHeader("*");
-        configuration.addAllowedMethod("*");
-        configuration.setAllowCredentials(true); // 자격 증명을 포함한 요청을 허용
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
-
-
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
