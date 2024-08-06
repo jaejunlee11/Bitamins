cdfbcd26677043858faa4b48a59a2362a3f7153f
김수영 <kswim8914@gmail.com>
2024-08-06 16:14:08 +0900

    controller에 미션 교체 메소드 추가

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/mission/controller/MissionController.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/mission/controller/MissionController.java
@@ -6,6 +6,9 @@ import com.saessakmaeul.bitamin.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @RestController
 @RequestMapping("/missions")
 @RequiredArgsConstructor
@@ -13,9 +16,10 @@ public class MissionController {
 
     private final MissionService missionService;
     private final JwtUtil jwtUtil;
+    private static final Logger logger = LoggerFactory.getLogger(MissionController.class);
 
     // 데일리 미션 조회
-    @GetMapping("")
+    @GetMapping
     public MissionResponse getMission(@RequestHeader(value = "Authorization", required = false) String tokenHeader){
         // ID 추출
         Long memberId = jwtUtil.extractUserId(tokenHeader.substring(7));
@@ -23,5 +27,14 @@ public class MissionController {
         // Service 호출
         MissionResponse missionResponse = missionService.readMission(memberId);
         return missionResponse;
+    }
+
+    // 미션 교체
+    @GetMapping("/substitute")
+    public MissionResponse getMissionSubstitute(@RequestParam("missionId") Long missionId){
+        // Service 호출
+        MissionResponse missionResponse = missionService.changeMission(missionId);
+        return missionResponse;
+
     }
 }
