3e3cbbcb96e3b72b1e0d95fe8e858f784dee1d41
김수영 <kswim8914@gmail.com>
2024-08-07 16:39:55 +0900

    feat: 미션 리뷰 등록 기능 추가

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/mission/controller/MissionController.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/mission/controller/MissionController.java
@@ -1,10 +1,15 @@
 package com.saessakmaeul.bitamin.mission.controller;
 
+import com.saessakmaeul.bitamin.mission.dto.request.MemberMissionRequest;
+import com.saessakmaeul.bitamin.mission.dto.response.MemberMissionResponse;
 import com.saessakmaeul.bitamin.mission.dto.response.MissionResponse;
 import com.saessakmaeul.bitamin.mission.service.MissionService;
 import com.saessakmaeul.bitamin.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/missions")
@@ -43,5 +48,21 @@ public class MissionController {
         // Service 호출
         MissionResponse missionResponse = missionService.completedMission(memberId, date);
         return missionResponse;
+    }
+
+    // 미션 리뷰 등록 기능
+    @PostMapping
+    public MemberMissionResponse postMission(@RequestHeader(value = "Authorization", required = false) String tokenHeader,
+                                             @RequestPart("memberMissionRequest") MemberMissionRequest memberMissionRequest,
+                                             @RequestPart("mssionImage") MultipartFile missionImage) throws IOException {
+        // ID 추출
+        Long memberId = jwtUtil.extractUserId(tokenHeader.substring(7));
+
+        // MemberMissionRequest에 missionImage 설정
+        memberMissionRequest.setMissionImage(missionImage);
+
+        // Service 호출
+        MemberMissionResponse memberMissionResponse = missionService.createMemberMission(memberId, memberMissionRequest);
+        return memberMissionResponse;
     }
 }
