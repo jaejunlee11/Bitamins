33b8183b0df0b67cd1011a200c58ff3745432536
조성정 <jsj046@naver.com>
2024-08-12 11:16:39 +0900

    hot fix: filter 무시의 무시

--- Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/filter/JwtFilter.java
+++ Backend/BITAMIN/src/main/java/com/saessakmaeul/bitamin/filter/JwtFilter.java
@@ -40,7 +40,7 @@ public class JwtFilter extends OncePerRequestFilter {
 
         logger.debug(requestURI + " " + method);
 
-        if(requestURI.startsWith("/api")) chain.doFilter(request, response);
+        // if(requestURI.startsWith("/api")) chain.doFilter(request, response);
 
         if(requestURI.startsWith("/api/auth/login") ||
                 requestURI.startsWith("/api/auth/kakao") ||
